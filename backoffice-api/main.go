e63f869b11a7dd29bae1dbfa430ae5b2ffc007e5
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-08-15 14:50:38 +0100

    init with multistage build

---
+++ backoffice-api/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	router.Use(cors.New(config))
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+			"service": "backoffice-api",
+		})
+	})
+
+	router.Run()
+}
---
+++ be/go.mod
@@ -0,0 +1,8 @@
+module be
+
+go 1.13
+
+require (
+	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/gin v1.6.3
+)
